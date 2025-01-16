dc6c7ee9734231a9a6f131e2c22dfeb334022b81
ViktorPetrina_ <viktor.petrina01@gmail.com>
2025-01-16 20:51:13 +0100

    first commit

---
+++ PatientManager/Migrations/20250111115336_Migration3.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatientManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_patient_patient_PatientId",
+                table: "patient");
+
+            migrationBuilder.DropIndex(
+                name: "IX_patient_PatientId",
+                table: "patient");
+
+            migrationBuilder.DropColumn(
+                name: "PatientId",
+                table: "patient");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "PatientId",
+                table: "patient",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_patient_PatientId",
+                table: "patient",
+                column: "PatientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_patient_patient_PatientId",
+                table: "patient",
+                column: "PatientId",
+                principalTable: "patient",
+                principalColumn: "id");
+        }
+    }
+}
---
+++ PatientManager/Models/Patient.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PatientManager.Models
+{
+    [Table("patient")]
+    public class Patient
+    {
+        [Column("id")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Column("first_name")]
+        public string? FirstName { get; set; }
+
+        [Column("last_name")]
+        public string? LastName { get; set; }
+
+        [Column("sex")]
+        public char Sex { get; set; }
+
+        [Column("date_of_birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Column("oib")]
+        [StringLength(11)]
+        public string? Oib { get; set; }
+    }
+}
